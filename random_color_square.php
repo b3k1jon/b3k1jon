43b3440e1f3b3d4c4c0cad1a1c25ef408d0db831
Bexruz Bekov <68767526+b3k1jon@users.noreply.github.com>
2020-10-30 10:24:48 +0500

    Creating 12 squares with random color
    Making 12 squares with using Php, Html, Css

---
+++ random_color_square.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+		<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<style>
+<?php
+
+	for ($a = 0; $a < 12; $a++) {
+$color = [
+"#000000", 
+"#607D8B", 
+"#708090", 
+"#8B4513", 
+"#F4A460", 
+"#FFEBCD"
+];
+	$c = rand(0,5);
+	$get = $color[$c];
+	
+$html .= ".square".$a." {
+	height: 50px;
+	width: 50px;
+	background-color:".$get."}";
+} 
+	echo $html;
+	
+?>
+	</style>
+		</head>
+	<body>
+<?php
+for ($b = 0; $b < 12; $b++) {
+	echo "<div class='square".$b."'></div>";
+}
+?>
+	</body>
+</html>
