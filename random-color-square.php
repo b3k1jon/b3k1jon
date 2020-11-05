e8a54c774b6b835b065808a09068798fcc58cc76
Bexruz Bekov <b3k1jon@gmail.com>
2020-11-05 21:15:45 +0500

    Update random-color-square.php

--- random-color-square.php
+++ random-color-square.php
@@ -4,20 +4,21 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
 	<style>
-<?php
+  <?php
 
 	for ($a = 0; $a < 12; $a++) {
-$color = [
+ $color = [
 "#000000", 
 "#607D8B", 
 "#708090", 
 "#8B4513", 
 "#F4A460", 
 "#FFEBCD"
-];
+ ];
 	$c = rand(0,5); //getting random number for $color massive
 	$get = $color[$c]; //putting $c 
-	
+		
+//replacing 12 squares css code with php
 $html .= ".square".$a." {
 	height: 50px;
 	width: 50px;
@@ -30,6 +31,8 @@ $html .= ".square".$a." {
 		</head>
 	<body>
 <?php
+
+//putting html, css code 12 times to create 12 squares
 for ($b = 0; $b < 12; $b++) {
 	echo "<div class='square".$b."'></div>";
 }
